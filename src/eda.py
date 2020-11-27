324c48ffa0b1a547c28a0c8b00317855dd6b9cab
Syad Khan <syad@student.ubc.ca>
2020-11-27 15:35:10 -0500

    Add the second figure

--- src/eda.py
+++ src/eda.py
@@ -7,15 +7,11 @@ Created on Fri Nov 27 13:29:53 2020
 """
 import pandas as pd
 import altair as alt
-alt.renderers.enable('altair_viewer')
-alt.data_transformers.disable_max_rows()
-
-#1984 - #1996
+#alt.renderers.enable('altair_viewer')
+#alt.data_transformers.disable_max_rows()
 
 df = pd.read_csv('../data/processed/ice_thickness.csv')
 df = df.groupby(["station_id", "station_name", "month", "year"]).mean().reset_index()
-
-#df = df.iloc[0:500]
 
 mean_thickness_year = alt.Chart(df).mark_bar(size=16).encode(
         y = alt.Y("median(ice_thickness)", title="Median Ice Thickness (cm)"),
@@ -23,9 +19,18 @@ mean_thickness_year = alt.Chart(df).mark_bar(size=16).encode(
     ).properties(
         background='white', title='Median Ice Thickness over Time'
     )
-
-
 mean_thickness_year
 
-#mean_thickness_year.save('filename.png')
-#mean_thickness_year.save('chart.png')
+
+(alt.Chart(df.query('(year == 1996 | year == 1984) & month == 2')).transform_density(
+    'ice_thickness',
+    groupby = ['year'],
+    as_=['ice_thickness','density']
+    )
+ .mark_area(opacity=0.4).encode(
+     x=alt.X('ice_thickness', title="Ice Thickness (cm)"),
+     y=alt.Y('density:Q', title="Density of Observations"),
+     color=alt.Color('year:O', scale=alt.Scale(scheme="set1")))
+ .properties(
+        background='white', title='Distribution of Ice Thickness for Analysis Month'
+    ))
