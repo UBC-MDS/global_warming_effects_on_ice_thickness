dec4092a23e1999e8233d3bff61b36b2b1c23acc
Syad Khan <syad@student.ubc.ca>
2020-11-28 02:31:01 -0500

    Add png files and update facet details

--- src/eda.py
+++ src/eda.py
@@ -24,29 +24,36 @@ opt = docopt(__doc__)
 def main(input_path, figure1, figure2):
     # read in data
     df = pd.read_csv(input_path)
-    df = df.groupby(["station_id", "station_name", "month", "year"]).mean().reset_index()
+    #df = df.groupby(["station_id", "station_name", "month", "year"]).mean().reset_index()
     
     # first plot of mean thickness by year
     mean_thickness_year = (alt.Chart(df).mark_bar(size=16).encode(
-            y = alt.Y("median(ice_thickness)", title="Median Ice Thickness (cm)"),
-            x = alt.X("year:O", title="Year"))
-        .properties(
-            background='white', title='Median Ice Thickness over Time'
-        ))
+        y = alt.Y("median(mean_ice_thickness)", title="Median Ice Thickness Averages(cm)"),
+        x = alt.X("year:O", title="Year"))
+    .properties(
+        background='white', title='Median Ice Thickness over Time'
+    ))
     
+    
+    # prepare dataframe for facetted (monthly) density plot
+    df_density = df.query('(year in [1984,1996]) & (month in [1,2,3])')
+    month_names = {1: "January", 2:"February", 3:"March"}
+    df_density = df_density.replace({"month":month_names})
     # second plot of thickness dsitribution for analysis years
-    density = (alt.Chart(df.query('(year in [1984,1996]) & (month in [1,2,3])'))
-   .transform_density(
-    'ice_thickness',
+    density = (alt.Chart(df_density)
+    .transform_density(
+    'mean_ice_thickness',
     groupby = ['year', 'month'],
-    as_=['ice_thickness','density']
+    as_=['mean_ice_thickness','density']
     )
-  .mark_area(opacity=0.4).encode(
-      x=alt.X('ice_thickness', title="Ice Thickness (cm)"),
-      y=alt.Y('density:Q', title="Density of Observations"),
-      color=alt.Color('year:O', scale=alt.Scale(scheme="set1")))
-  .properties(title='Distribution of Ice Thickness for Analysis Month')
-  .facet("month", background='white'))
+    .mark_area(opacity=0.4).encode(
+       x=alt.X('mean_ice_thickness:Q', title="Ice Thickness (cm)"),
+       y=alt.Y('density:Q', title="Density of Observations"),
+       color=alt.Color('year:O', scale=alt.Scale(scheme="set1")))
+    .facet(column=alt.Row("month",
+                          title='Distribution of Ice Thickness by Month',
+                          sort=['January','February','March']), 
+           background='white'))
     
     # save files
     save(mean_thickness_year, figure1)
