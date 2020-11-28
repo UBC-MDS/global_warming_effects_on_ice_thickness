5c928da3d44ad1b1fc5cef8896626a1c301bc904
Syad Khan <syad@student.ubc.ca>
2020-11-28 12:44:18 -0500

    Change docopt arguments to only input and output path and update functions to use these

--- src/eda.py
+++ src/eda.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+# author: Syad Khan
+# date: 2020-11-27
 
 """This script reads processed data from a specified input filepath and produces EDA figures to
 to a specificed output filepath
-Usage: test.py <input_path> --figure1=<figure1> --figure2=<figure2>
+Usage: test.py <input_path> <output_path>
 
 Options:
-<input_path>       Path to the data file
---figure1=<figure1>
---figure2=<figure2>
+<input_path>        Path to the input data file
+<output_path>       Path to save figures to
 """
 
 import pandas as pd
@@ -21,43 +22,47 @@ import chromedriver_binary
 
 opt = docopt(__doc__)
 
-def main(input_path, figure1, figure2):
+def main(input_path, output_path):
     # read in data
     df = pd.read_csv(input_path)
     
-    # first plot of mean thickness by year
+    # first figure of mean thickness by year
     mean_thickness_year = (alt.Chart(df).mark_bar(size=16).encode(
         y = alt.Y("median(mean_ice_thickness)", title="Median Ice Thickness Averages(cm)"),
         x = alt.X("year:O", title="Year"))
-    .properties(
-        background='white', title='Median Ice Thickness over Time'
-    ))
+    .properties(background='white', 
+                title='Median Ice Thickness over Time'))
     
     
+    # second figure of thickness dsitribution for analysis years 
     # prepare dataframe for facetted (monthly) density plot
     df_density = df.query('(year in [1984,1996]) & (month in [1,2,3])')
     month_names = {1: "January", 2:"February", 3:"March"}
     df_density = df_density.replace({"month":month_names})
-    # second plot of thickness dsitribution for analysis years
-    density = (alt.Chart(df_density)
-    .transform_density(
-    'mean_ice_thickness',
-    groupby = ['year', 'month'],
-    as_=['mean_ice_thickness','density']
-    )
-    .mark_area(opacity=0.4).encode(
-       x=alt.X('mean_ice_thickness:Q', title="Ice Thickness (cm)"),
-       y=alt.Y('density:Q', title="Density of Observations"),
-       color=alt.Color('year:O', scale=alt.Scale(scheme="set1")))
-    .facet(column=alt.Row("month",
-                          title='Distribution of Ice Thickness by Month',
-                          sort=['January','February','March']), 
-           background='white'))
     
-    # save files
-    save(mean_thickness_year, figure1)
-    save(density, figure2)
+    # create figure
+    density = (alt.Chart(df_density)
+        .transform_density(
+            'mean_ice_thickness',
+            groupby = ['year', 'month'],
+            as_=['mean_ice_thickness','density']
+        )
+        .mark_area(opacity=0.4).encode(
+           x=alt.X('mean_ice_thickness:Q', title="Ice Thickness (cm)"),
+           y=alt.Y('density:Q', title="Density of Observations"),
+           color=alt.Color('year:O', scale=alt.Scale(scheme="set1")))
+        .facet(column=alt.Row("month",
+                              title='Distribution of Ice Thickness by Month',
+                              sort=['January','February','March']), 
+               background='white'))
+    
+    # save first figure
+    figure_1_path = output_path + '/median_thickness_year.png'
+    save(mean_thickness_year, figure_1_path)
+    # save second figure
+    figure_2_path = output_path + '/density.png'
+    save(density, figure_2_path)
         
 if __name__ == "__main__":
-    main(opt["<input_path>"], opt["--figure1"], opt["--figure2"])
+    main(opt["<input_path>"], opt["<output_path>"])
     
