8fb1512db06ab3f95c14749362f09e167aac504c
Syad Khan <syad@student.ubc.ca>
2020-11-28 02:07:25 -0500

    Add docopt funtionality and png saving from altair

--- src/eda.py
+++ src/eda.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"""This script outputs the input variable
+"""This script takes in a filepath.
+This script outputs figures
 Usage: test.py <var1> --figure1=<figure1> --figure2=<figure2>
 
 Options:
@@ -13,6 +14,10 @@ Options:
 import pandas as pd
 import altair as alt
 from docopt import docopt
+from altair_saver import save
+#from selenium import webdriver
+import chromedriver_binary
+#driver = webdriver.Chrome()
 
 opt = docopt(__doc__)
 
@@ -30,22 +35,22 @@ def main(input_path, figure1, figure2):
         ))
     
     # second plot of thickness dsitribution for analysis years
-    density = (alt.Chart(df.query('(year == 1996 | year == 1984) & month == 2')).transform_density(
-        'ice_thickness',
-        groupby = ['year'],
-        as_=['ice_thickness','density']
-        )
-      .mark_area(opacity=0.4).encode(
-          x=alt.X('ice_thickness', title="Ice Thickness (cm)"),
-          y=alt.Y('density:Q', title="Density of Observations"),
-          color=alt.Color('year:O', scale=alt.Scale(scheme="set1")))
-      .properties(
-            background='white', title='Distribution of Ice Thickness for Analysis Month'
-        ))
+    density = (alt.Chart(df.query('(year in [1984,1996]) & (month in [1,2,3])'))
+   .transform_density(
+    'ice_thickness',
+    groupby = ['year', 'month'],
+    as_=['ice_thickness','density']
+    )
+  .mark_area(opacity=0.4).encode(
+      x=alt.X('ice_thickness', title="Ice Thickness (cm)"),
+      y=alt.Y('density:Q', title="Density of Observations"),
+      color=alt.Color('year:O', scale=alt.Scale(scheme="set1")))
+  .properties(title='Distribution of Ice Thickness for Analysis Month')
+  .facet("month", background='white'))
     
     # save files
-    mean_thickness_year.save(figure1)
-    density.save(figure2)
+    save(mean_thickness_year, figure1)
+    save(density, figure2)
         
 if __name__ == "__main__":
     main(opt["<var1>"], opt["--figure1"], opt["--figure2"])
